a293eee03a7fc18b63ea84a8d9348bec12283486
Niwdett <164444242+Niwdett@users.noreply.github.com>
2024-05-03 07:58:20 -0500

    Update Main.java

--- Parcial2/src/Main.java
+++ Parcial2/src/Main.java
@@ -2,9 +2,7 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                // Aquí creas una instancia de tu ventana principal o de inicio de sesión
-                // dependiendo de la lógica de tu aplicación.
-                // Por ejemplo:
+            
                 VentanaInicioSesion ventanaInicioSesion = new VentanaInicioSesion();
                 ventanaInicioSesion.mostrar();
             }
